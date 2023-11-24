2ec5b2e69662bc85b2218d8cf66fad4364b0672d
Pritam Das <pritamd47@gmail.com>
2023-11-24 12:02:16 -0800

    cleanup network generation code

--- src/resorr/generate_network.py
+++ src/resorr/generate_network.py
@@ -1,47 +1,74 @@
-# %%
+from pathlib import Path
 import rioxarray as rxr
 import xarray as xr
-from rasterio.plot import plotting_extent
-from rasterio.transform import xy
 import numpy as np
 import geopandas as gpd
-import cartopy.crs as ccrs
-import cartopy
-from pyproj import Transformer
-import matplotlib.pyplot as plt
-from collections import OrderedDict
-from pathlib import Path
-from shapely.geometry import Point, LineString
-from itertools import pairwise
+import networkx as nx
+import geonetworkx as gnx
+
 
 import warnings
 warnings.simplefilter('ignore')
 
-def generate_network(flow_dir_fn, stations_fn, DIST_PROJ, elevation_fn=None):
+def generate_network(
+        flow_dir_fn, 
+        stations_fn, 
+        save_dir, 
+        dist_proj=None, 
+        elevation_fn=None
+    ) -> (gpd.GeoDataFrame, gpd.GeoDataFrame): 
+    """generate reservoir network using flow direction file and reservoir locations.
+
+    Args:
+        flow_dir_fn (str or Path): path to flow direction file
+        stations_fn (str or Path): path to reservoir locations file
+        save_dir (str or Path): path to save directory
+        dist_proj (str, optional): projection to use for optionally calculating distance between reservoirs. Defaults to None.
+        elevtion_fn (str or Path, optional): path to elevation file used for optionally adding elevation data to each reservoir. Defaults to None.
+
+    Returns:
+        (gpd.GeoDataFrame, gpd.GeoDataFrame): tuple of (edges, nodes) GeoDataFrames
+    """
+
+    # check if files exist
+    flow_dir_fn = Path(flow_dir_fn)
+    assert flow_dir_fn.exists()
+
+    stations_fn = Path(stations_fn)
+    assert stations_fn.exists()
+
+    save_dir = Path(save_dir)
+    if not save_dir.exists():
+        print(f"passed save_dir does not exist, creating {save_dir}")
+        save_dir.mkdir(parents=True)
+    
+    if elevation_fn is not None:
+        elevation_fn = Path(elevation_fn)
+        assert elevation_fn.exists()
+
     fdr = rxr.open_rasterio(flow_dir_fn, masked=True)
     band = fdr.sel(band=1)
 
     band_vicfmt = band
 
-    # create id of each cell
-    band_ids = np.full_like(band_vicfmt, np.nan)
-    _id = 0
-    for i in np.arange(band_vicfmt.shape[0]):
-        for j in np.arange(band_vicfmt.shape[1]):
-            if not np.isnan(band_vicfmt[i][j]):
-                band_ids[i][j] = _id
-                _id += 1
-    # create nodes
-    # extract x-y points
-    xs, _ = xy(fdr.rio.transform(), 0, np.arange(fdr.rio.width))
-    _, ys = xy(fdr.rio.transform(), np.arange(fdr.rio.height), 9)
+    reservoirs = gpd.read_file(stations_fn)
+    reservoirs['geometry'] = gpd.points_from_xy(reservoirs['lon'], reservoirs['lat'])
+    reservoirs.set_crs('epsg:4326', inplace=True)
 
-    XX, YY = np.meshgrid(xs, ys)
+    reservoir_location_raster = xr.full_like(band_vicfmt, np.nan)
+    for resid, row in reservoirs.iterrows():
+        reslat = float(row.lat)
+        reslon = float(row.lon)
 
-    # create gdf
-    grid_pts = gpd.GeoDataFrame(data={'id': band_ids.flatten()}, geometry=[Point(x, y) for x, y in zip(XX.flatten(), YY.flatten())], crs='EPSG:4326')
-    grid_pts['to_id'] = np.nan
-    # create id of each cell
+        rast_lat = reservoir_location_raster.indexes['y'].get_indexer([reslat], method="nearest")[0]
+        rast_lon = reservoir_location_raster.indexes['x'].get_indexer([reslon], method="nearest")[0]
+
+        reservoir_location_raster[rast_lat, rast_lon] = resid
+
+    # convert all points to nodes. Use index value to identify
+    G = gnx.GeoDiGraph()
+    G.add_nodes_from(reservoirs.index)
+
     operations = {
         1: [-1, 0],  # N
         2: [-1, 1],  # NE
@@ -53,140 +80,88 @@ def generate_network(flow_dir_fn, stations_fn, DIST_PROJ, elevation_fn=None):
         8: [-1, -1], # NW
     }
 
-    to_band_ids = np.full_like(band_vicfmt, np.nan)
-    for x in np.arange(band_vicfmt.shape[0]):   
-        for y in np.arange(band_vicfmt.shape[1]):  
-            if not np.isnan(band_vicfmt[x][y]):
-                if isinstance(band_vicfmt, xr.DataArray):
-                    direction = int(band_vicfmt[x][y].values)
-                else:
-                    direction = band_vicfmt[x][y]
-                op = operations[direction]
-                new_x, new_y = x + op[0], y + op[1]
-                if new_x < band_ids.shape[0] and new_y < band_ids.shape[1]:
-                    # print(band_ids[x, y], (x, y), directions[direction], " > | >", band_ids[new_x, new_y], (new_x, new_y))
-                    to_band_ids[x, y] = band_ids[new_x, new_y]
-                else:
-                    # print(band_ids[x, y], (x, y), directions[direction], " > | >", np.nan, (new_x, new_y))
-                    to_band_ids[x, y] = np.nan
-
-    grid_pts['to_id'] = to_band_ids.flatten()
-
-    grid_pts.dropna(inplace=True)
-
-    # Lets import the reservoir locations
-    reservoirs = gpd.read_file(stations_fn)
-    reservoirs['geometry'] = gpd.points_from_xy(reservoirs['lon'], reservoirs['lat'])
-    reservoirs.set_crs('epsg:4326', inplace=True)
-
-    reservoirs_to_join = reservoirs[['name', 'lon', 'lat', 'geometry']]
-
-    if elevation_fn is not None:
-        elevation = rxr.open_rasterio(elevation_fn)
-        transformer = Transformer.from_crs("EPSG:4326", elevation.rio.crs, always_xy=True)
-        indices = [transformer.transform(lon, lat) for (lon, lat) in zip(reservoirs['geometry'].x, reservoirs['geometry'].y)]
-        reservoirs_to_join['elevation'] = [elevation.sel(x=xx, y=yy, method="nearest").values[0] for xx, yy in indices]
-
-    grid_pts_joined_stns = grid_pts.to_crs(DIST_PROJ).sjoin_nearest(reservoirs_to_join.to_crs(DIST_PROJ), how='left', distance_col='dist [m]').to_crs('epsg:4326')
-    # TODO: give a warning if multiple stations are within the same cell
-
-    grid_pts_joined_stns['min_dist'] = grid_pts_joined_stns.groupby('name')['dist [m]'].transform('min')
-    grid_pts_joined_stns.loc[grid_pts_joined_stns['dist [m]'] != grid_pts_joined_stns['min_dist'], ['index_right', 'name', 'lon', 'lat', 'dist [m]', 'min_dist', 'elevation']] = np.nan
-
-    # save the root index
-    root = None
-    # Arrows
-    for i, row in grid_pts.iterrows():
-        from_loc = row['geometry']
-        try:
-            to_loc = grid_pts.loc[grid_pts['id'] == row['to_id']].iloc[0]['geometry']
-        except IndexError:
-            to_loc = from_loc
-            root = row
-
-        x, y = from_loc.x, from_loc.y
-        dx, dy = to_loc.x - x, to_loc.y - y
-
-    grid_pts_joined_stns = grid_pts_joined_stns.to_crs(DIST_PROJ)
-    stations = grid_pts_joined_stns.dropna()
-
-    dist = lambda left_pt, right_pt: np.sqrt((left_pt.x-right_pt.x)**2 + (left_pt.y-right_pt.y)**2)
-
-    network = OrderedDict()
-    for i, stn in stations.iterrows():
-        id = stn['index_right']
-        length = 0
+    for node in G.nodes:
+        resdata = reservoirs[reservoirs.index==node]
         
-        if id not in network:
-            network[id] = OrderedDict([
-                ('network', []),
-                ('lengths', [])
-            ])
+        x = float(resdata['lon'].values[0])
+        y = float(resdata['lat'].values[0])
 
-        current_id = stn['id']
-        while True:
-            current_node = grid_pts_joined_stns.loc[grid_pts_joined_stns['id']==current_id]
-            next_id = current_node['to_id'].iloc[0]
-            next_node = grid_pts_joined_stns.loc[grid_pts_joined_stns['id']==next_id]
-            # print("\n\n\n", current_node['geometry'], next_node['geometry'])
-            if len(next_node) > 0:
-                length += dist(current_node['geometry'].iloc[0], next_node['geometry'].iloc[0])
-            if not np.isnan(current_node['index_right'].iloc[0]):
-                network[id]['network'].append(current_node['index_right'].iloc[0])
-                network[id]['lengths'].append(length)
+        idxx = band_vicfmt.indexes['x'].get_indexer([x], method="nearest")[0]
+        idxy = band_vicfmt.indexes['y'].get_indexer([y], method="nearest")[0]
+        
+        # travel downstream until another node, np.nan or out-of-bounds is found, or if travelling in a loop
 
-            if current_id == root['id']:
-                break
+        visited = [(idxx, idxy)]
+        current_pix = band_vicfmt.isel(x=idxx, y=idxy)
 
-            current_id = next_id
+        attrs_n = {
+            node: {
+                'x': reservoirs['geometry'][node].x,
+                'y': reservoirs['geometry'][node].y,
+                'name': reservoirs['name'][node]
+            }
+        }
+        nx.set_node_attributes(G, attrs_n)
 
-    grid_pts_joined_stns = grid_pts_joined_stns.to_crs(DIST_PROJ)
+        if not np.isnan(current_pix):
+            END = False
+            while not END:
+                op = operations[int(current_pix)]
+                new_idxy, new_idxx = np.array((idxy, idxx)) + np.array(op)
+                idxy, idxx = new_idxy, new_idxx
+                
+                if (new_idxx, new_idxy) in visited:
+                    # In a loop, exit
+                    END=True
+                    break
+                else:
+                    visited.append((new_idxx, new_idxy))
 
-    from_id = []
-    to_id = []
-    from_name = []
-    to_name = []
-    geoms = []
-    lengths = []
-    delta_elevs = []
-    slopes = []
+                current_pix = band_vicfmt.isel(x=new_idxx, y=new_idxy)
+                if np.isnan(current_pix):
+                    # NaN value found, exit loop
+                    END=True
+                    break
 
-    for id in network:
-        downstreams = network[id]['network']
-        link_lengths = network[id]['lengths']
+                try:
+                    any_reservoir = reservoir_location_raster.isel(x=new_idxx, y=new_idxy)
+                    if not np.isnan(any_reservoir):
+                        # another reservoir found
+                        G.add_edge(node, int(any_reservoir))
+                        if dist_proj:
+                            attrs_e = {
+                                (node, int(any_reservoir)): {
+                                    'length': reservoirs.to_crs(dist_proj)['geometry'][node].distance(reservoirs.to_crs(dist_proj)['geometry'][int(any_reservoir)])
+                                }
+                            }
+                            nx.set_edge_attributes(G, attrs_e)
+                        END = True
+                        break
+                except IndexError:
+                    # Reached end
+                    END=True
 
-        for id_pair, length in zip(pairwise(downstreams), link_lengths[1:]):  # the first element of `link_lengths` is garbage value. It is the distance of a single link
-            start_res = grid_pts_joined_stns.loc[grid_pts_joined_stns['index_right']==id_pair[0]].to_crs('epsg:4326').iloc[0]  # Get the points in EPSG:4326 CRS (lat-long)
-            end_res = grid_pts_joined_stns.loc[grid_pts_joined_stns['index_right']==id_pair[1]].to_crs('epsg:4326').iloc[0]  # Get the points in EPSG:4326 CRS (lat-long)
+    G_gdf = gpd.GeoDataFrame(gnx.graph_edges_to_gdf(G))
+    G_gdf_pts = gpd.GeoDataFrame(gnx.graph_nodes_to_gdf(G))
 
-            link = LineString([Point(pt.x, pt.y) for pt in (start_res.geometry, end_res.geometry)])
+    # add elevation data to nodes
+    if elevation_fn:
+        elev = rxr.open_rasterio(elevation_fn, chunks='auto')
 
-            geoms.append(link)
-            from_id.append(id_pair[0])
-            to_id.append(id_pair[1])
-            from_name.append(start_res['name'])
-            to_name.append(end_res['name'])
-            lengths.append(length)
-            delta_elevs.append(end_res['elevation']-start_res['elevation'])
-            slopes.append((end_res['elevation']-start_res['elevation'])/length)
+        G_gdf_pts['elevation'] = G_gdf_pts[['x', 'y']].apply(lambda row: float(elev.sel(x=row.x, y=row.y, method='nearest')), axis=1)
+        G_gdf_pts.head()
 
-    res_network = gpd.GeoDataFrame(data={
-        'from_id': from_id,
-        'to_id': to_id,
-        'from_name': from_name,
-        'to_name': to_name,
-        'geometry': geoms,
-        'length': lengths,
-        'delta_elevation': delta_elevs,
-        'slope': slopes
-    })
+    pts_save_fn = Path(save_dir) / 'rivreg_network_pts.shp'
+    edges_save_fn = Path(save_dir) / 'rivreg_network.shp'
+    
+    G_gdf_pts.to_file(pts_save_fn)
+    G_gdf.to_file(edges_save_fn)
 
-    # Save `res_network` for further use
-    return res_network, grid_pts_joined_stns.dropna().to_crs('epsg:4326')
+    return G
 
 
 def main():
-    DIST_PROJ = "+proj=eqdc +lon_0=-103.7988281 +lat_1=35.7127609 +lat_2=43.8942567 +lat_0=39.8035088 +datum=WGS84 +units=m +no_defs"
+    dist_proj = "+proj=eqdc +lon_0=-103.7988281 +lat_1=35.7127609 +lat_2=43.8942567 +lat_0=39.8035088 +datum=WGS84 +units=m +no_defs"
     flow_dir_fn = Path("data/colorado/basins/gunnison/ro/pars/fl.tif")
     stations_fn = Path("data/gunnison_reservoirs/gunnison_reservoirs_fdr_corrected_usgs_gages.csv")
     elevation_fn = Path("global_data/global_elevation_data/World_e-Atlas-UCSD_SRTM30-plus_v8.tif")
@@ -197,7 +172,7 @@ def main():
     assert stations_fn.exists()
     assert elevation_fn.exists()
 
-    network, network_pts = generate_network(flow_dir_fn, stations_fn, DIST_PROJ, elevation_fn)
+    network, network_pts = generate_network(flow_dir_fn, stations_fn, dist_proj, elevation_fn)
 
     network.to_file(save_fn)
     network_pts.to_file(save_fn.with_name('rivreg_network_pts.shp'))
